abd133dedcb154b8a68dfda7869b23b5d5e765e7
Tjaša <tjasazzeleznik@gmail.com>
2025-07-03 11:43:45 +0200

    Changed new entry form
    The user can now select a date (and if they don't, it's automatically set to today). The 'date' column in the 'entry' table now had the 'date' datatype

--- server.js
+++ server.js
@@ -234,20 +234,21 @@ app.post('/login', (req, res)=> {
 });
 
 app.post('/newentry', (req, res) => {
-	const {title, content} = req.body;
+	const {date, title, content} = req.body;
 	if (!content || content.trim() === '') {
 		return res.render('index.ejs', { err: 'Content cannot be empty' });
 	}
 	const userId = req.session.userId;
-	const now = new Date().toISOString().slice(0, 19).replace('T', ' ');
+	const today = new Date().toISOString().slice(0, 10);
+	const entryDate = date || today;
 	pool.query(
-		`INSERT INTO Entry (User_Id, Title, Content, Date) values (?, ?, ?, ?) `, [userId, title, content, now], (err) => {
+		`INSERT INTO Entry (User_Id, Title, Content, Date) values (?, ?, ?, ?) `, [userId, title, content, entryDate], (err) => {
 			if(err) {
 				console.error("Error inserting entry in the database:", err);
 				return res.render('index.ejs', {entries: [], err: err.message, activePage: 'dashboard'})
 			}
 			console.log('Entry added successfully!');
-			return res.redirect('/entries', {err: "", activePage: 'entries'});
+			return res.redirect('/entries');
 		}
 	);
 });
